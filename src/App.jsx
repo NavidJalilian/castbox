1200491928686bdebed64484dd2407c62307e972
Navid Jalilian <n.jalilian@aranumadc.local>
2024-03-07 13:55:04 +0330

    add shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
-import Profile from './components/Profile';
-import UserProfileList from './components/UserProfileList';
-import UserProfileDetail from './components/UserProfileDetail';
-import CreatePost from './components/CreatePost';
-import PostsFeed from './components/PostsFeed';
-import PrivateRoute from './components/PrivateRoute';
-import ChatPage from './components/ChatPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Profile from "./components/Profile";
+import UserProfileList from "./components/UserProfileList";
+import UserProfileDetail from "./components/UserProfileDetail";
+import CreatePost from "./components/CreatePost";
+import PostsFeed from "./components/PostsFeed";
+import PrivateRoute from "./components/PrivateRoute";
+import ChatPage from "./components/ChatPage";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<div>Home Page</div>} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/' element={<Home />} />
 
         <Route element={<PrivateRoute />}>
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profiles" element={<UserProfileList />} />
-          <Route path="/profiles/:username" element={<UserProfileDetail />} />
-          <Route path="/create-post" element={<CreatePost />} />
-          <Route path="/feed" element={<PostsFeed />} />
-          <Route path="/chat/:username" element={<ChatPage />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profiles' element={<UserProfileList />} />
+          <Route path='/profiles/:username' element={<UserProfileDetail />} />
+          <Route path='/create-post' element={<CreatePost />} />
+          <Route path='/feed' element={<PostsFeed />} />
+          <Route path='/chat/:username' element={<ChatPage />} />
         </Route>
-
       </Routes>
     </Router>
   );
